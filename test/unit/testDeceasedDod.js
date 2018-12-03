270c45bc8e0c14e9c03314b6231dbc2ed9c21ef7
Luca Del Buono <LucaDelBuono@users.noreply.github.com>
2018-12-03 15:25:08 +0000

    PRO-4149: Remove unused methods

--- test/unit/testDeceasedDod.js
+++ test/unit/testDeceasedDod.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const initSteps = require('app/core/initSteps');
-const {expect, assert} = require('chai');
+const expect = require('chai').expect;
 const steps = initSteps([`${__dirname}/../../app/steps/action/`, `${__dirname}/../../app/steps/ui`]);
 const DeceasedDod = steps.DeceasedDod;
 
@@ -10,30 +10,6 @@ describe('DeceasedDod', () => {
         it('should return the correct url', (done) => {
             const url = DeceasedDod.constructor.getUrl();
             expect(url).to.equal('/deceased-dod');
-            done();
-        });
-    });
-
-    describe('handlePost()', () => {
-        let ctx;
-        let errors;
-        let formdata;
-        let session;
-        let hostname;
-
-        it('should return the ctx with the deceased dod', (done) => {
-            ctx = {
-                dob_day: '02',
-                dob_month: '03',
-                dob_year: '1952'
-            };
-            errors = {};
-            [ctx, errors] = DeceasedDod.handlePost(ctx, errors, formdata, session, hostname);
-            expect(ctx).to.deep.equal({
-                dob_day: '02',
-                dob_month: '03',
-                dob_year: '1952',
-            });
             done();
         });
     });
