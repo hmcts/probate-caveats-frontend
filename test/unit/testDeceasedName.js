31c7fa13e60eb8c227ab04cdfd1a79f1384a1b39
Wivenhoe-HMCTS <jason.smith@HMCTS.NET>
2019-01-08 09:29:45 +0000

    PRO-4147: test for deceased name

--- test/unit/testDeceasedName.js
+++ test/unit/testDeceasedName.js
@@ -1,14 +1,12 @@
 'use strict';
 
-const initSteps = require('app/core/initSteps');
-const expect = require('chai');
-const steps = initSteps([`${__dirname}/../../app/steps/action/`, `${__dirname}/../../app/steps/ui`]);
-const DeceasedName = steps.DeceasedName;
+const expect = require('chai').expect;
+const DeceasedName = require('app/steps/ui/deceased/name/index');
 
 describe('DeceasedName', () => {
     describe('getUrl()', () => {
         it('should return the correct url', (done) => {
-            const url = DeceasedName.constructor.getUrl();
+            const url = DeceasedName.getUrl();
             expect(url).to.equal('/deceased-name');
             done();
         });
