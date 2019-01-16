81132658886fd4594d1c776b5ba53a0d587ab88e
Wivenhoe-HMCTS <jason.smith@HMCTS.NET>
2019-01-16 11:42:10 +0000

    PRO-4148: add deceased address to caveat journey

--- test/unit/testDeceasedAddress.js
+++ test/unit/testDeceasedAddress.js
@@ -4,12 +4,30 @@ const initSteps = require('app/core/initSteps');
 const expect = require('chai').expect;
 const steps = initSteps([`${__dirname}/../../app/steps/action/`, `${__dirname}/../../app/steps/ui`]);
 const DeceasedAddress = steps.DeceasedAddress;
+const he = require('he');
 
 describe('DeceasedAddress', () => {
     describe('getUrl()', () => {
         it('should return the correct url', (done) => {
             const url = DeceasedAddress.constructor.getUrl();
             expect(url).to.equal('/deceased-address');
+            done();
+        });
+    });
+
+    describe('generateContent()', () => {
+        it('should replace content varaibles', (done) => {
+            const ctx = {};
+            const formdata = {
+                deceased: {
+                    firstName: 'Jason',
+                    lastName: 'Smith'
+                }
+            };
+            const content = DeceasedAddress.generateContent(ctx, formdata);
+            expect(he.decode(content.question)).to.equal(
+                'What was the permanent address of Jason Smith?'
+            );
             done();
         });
     });
