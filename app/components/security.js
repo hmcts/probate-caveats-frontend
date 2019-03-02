bc5966083bd5a91fdc3cc22c356b6953b32a9ff6
Wivenhoe-HMCTS <jason.smith@HMCTS.NET>
2019-03-02 17:55:44 +0000

    PRO-4152: update code for redirect_url

--- app/components/security.js
+++ app/components/security.js
@@ -26,7 +26,7 @@ const getOauth2Code = () => {
     const redirect_uri = config.services.idam.caveat_redirectUrl;
     const username = config.services.idam.caveat_user_email;
     const userpassword = config.services.idam.caveat_user_password;
-
+    logInfo('redirect_url: ' + redirect_uri);
     const headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Authorization': `Basic ${new Buffer(`${username}:${userpassword}`).toString('base64')}`
