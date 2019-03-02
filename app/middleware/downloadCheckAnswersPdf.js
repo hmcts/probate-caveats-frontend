78df3e096f2738efedb6d03c6461e15a19902e26
Wivenhoe-HMCTS <jason.smith@HMCTS.NET>
2019-03-02 12:45:08 +0000

    PRO-4152: update code for redirect_url

--- app/middleware/downloadCheckAnswersPdf.js
+++ app/middleware/downloadCheckAnswersPdf.js
@@ -2,12 +2,10 @@
 
 const commonContent = require('app/resources/en/translation/common');
 const pdfservices = require('app/components/pdf-services');
-const security = require('app/components/security');
 
 const downloadCheckAnswersPdf = (req, res) => {
     const formdata = req.session.form;
-    const redirect_url = security.getRedirectUrl(req);
-    pdfservices.createCheckAnswersPdf(formdata, redirect_url, req.session.id)
+    pdfservices.createCheckAnswersPdf(formdata, req.session.id)
         .then(result => {
             res.setHeader('Content-Type', 'application/pdf');
             res.setHeader('Content-disposition', 'attachment; filename=checkYourAnswers.pdf');
