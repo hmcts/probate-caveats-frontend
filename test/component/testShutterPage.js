0f703ebc88ad4d2284bc184dabfac635f21a0cda
Luca Del Buono <luca.delbuono@gmail.com>
2019-04-05 14:53:12 +0100

    Mock FT on component test

--- test/component/testShutterPage.js
+++ test/component/testShutterPage.js
@@ -1,21 +1,34 @@
 'use strict';
 
 const TestWrapper = require('test/util/TestWrapper');
+const config = require('app/config');
+
+const nock = require('nock');
+const featureToggleUrl = config.featureToggles.url;
+const shutterFeatureTogglePath = `${config.featureToggles.path}/${config.featureToggles.caveats_shutter_toggle}`;
+const featureTogglesNock = (status = 'true') => {
+    nock(featureToggleUrl)
+        .get(shutterFeatureTogglePath)
+        .reply(200, status);
+};
 
 describe('shutter-page', () => {
     let testWrapper;
 
     beforeEach(() => {
         testWrapper = new TestWrapper('ShutterPage');
+        featureTogglesNock('true');
     });
 
     afterEach(() => {
         testWrapper.destroy();
+        nock.cleanAll();
     });
 
     describe('Verify Content, Errors and Redirection', () => {
         it('test correct content loaded on the page', (done) => {
-            testWrapper.testContent(done, [], {});
+            const contentToExclude = ['paragraph3'];
+            testWrapper.testContent(done, contentToExclude, {});
         });
     });
 });
