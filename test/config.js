952d59894e99e46853ca6b0cf28de8ac1c8d188a
reddy1praveen <32567752+reddy1praveen@users.noreply.github.com>
2019-04-10 22:33:01 +0100

    Update config.js

--- test/config.js
+++ test/config.js
@@ -2,7 +2,7 @@ module.exports = {
 
     TestIdamBaseUrl: process.env.IDAM_API_URL || 'https://idam-api.aat.platform.hmcts.net',
     TestFrontendUrl: process.env.TEST_URL || 'http://localhost:3000',
-    TestE2EFrontendUrl: process.env.TEST_E2E_URL ||'https://probate-caveats-fe-aat.service.core-compute-aat.internal/caveats',
+    TestE2EFrontendUrl: process.env.TEST_E2E_URL ||process.env.TEST_URL,
     TestUseSidam: process.env.USE_SIDAM || 'true',
     TestUseGovPay: process.env.USE_GOV_PAY || 'true',
     TestInviteIdListUrl: process.env.INVITE_ID_LIST_URL || '/inviteIdList',
