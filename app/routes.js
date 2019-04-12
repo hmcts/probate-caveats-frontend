0c3bb2037d73b250cf7831e7764490a9044fdd53
Wivenhoe-HMCTS <43378735+Wivenhoe-HMCTS@users.noreply.github.com>
2019-04-12 14:10:13 +0100

    Update routes.js
    PRO-4946: remove duplicate route definitions

--- app/routes.js
+++ app/routes.js
@@ -64,46 +64,6 @@ router.get('/*', (req, res, next) => {
     }
 });
 
-router.get('/*', (req, res, next) => {
-    const formdata = req.session.form;
-    if (!includes(config.whitelistedPagesForStartPageRedirect, req.originalUrl) &&
-        !get(formdata, 'applicant')) {
-        res.redirect('/start-page');
-    } else {
-        next();
-    }
-});
-
-router.get('/*', (req, res, next) => {
-    const formdata = req.session.form;
-    if (!includes(config.whitelistedPagesForStartPageRedirect, req.originalUrl) &&
-        !get(formdata, 'applicant')) {
-        res.redirect('/start-page');
-    } else {
-        next();
-    }
-});
-
-router.get('/*', (req, res, next) => {
-    const formdata = req.session.form;
-    if (!includes(config.whitelistedPagesForStartPageRedirect, req.originalUrl) &&
-        !get(formdata, 'applicant')) {
-        res.redirect('/start-page');
-    } else {
-        next();
-    }
-});
-
-router.get('/*', (req, res, next) => {
-    const formdata = req.session.form;
-    if (!includes(config.whitelistedPagesForStartPageRedirect, req.originalUrl) &&
-        !get(formdata, 'applicant')) {
-        res.redirect('/start-page');
-    } else {
-        next();
-    }
-});
-
 const steps = initSteps([`${__dirname}/steps/action/`, `${__dirname}/steps/ui/`]);
 
 Object.entries(steps).forEach(([, step]) => {
