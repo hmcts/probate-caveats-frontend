d667b36c4338cc7c8f7d791c6b9818bfc0e6e678
Wivenhoe-HMCTS <jason.smith@HMCTS.NET>
2019-05-08 09:28:07 +0100

    PRO-5065: update jquery to cheerio

--- app/utils/CheckAnswersSummaryJSONObjectBuilder.js
+++ app/utils/CheckAnswersSummaryJSONObjectBuilder.js
@@ -1,26 +1,27 @@
 'use strict';
 
-const jsdom = require('jsdom');
-const {JSDOM} = jsdom;
+const cheerio = require('cheerio');
 
 class CheckAnswersSummaryJSONObjectBuilder {
 
     build(html) {
-        const dom = new JSDOM(html);
-        const $ = (require('jquery'))(dom.window);
+        const $ = cheerio.load(html);
         const summary = {};
         summary.sections = [];
         const sections = $('.heading-large, .heading-medium, .heading-small, .check-your-answers__row');
         const mainParagraph = $('#main-heading-content');
-        summary.mainParagraph = mainParagraph.html();
+        summary.mainParagraph = mainParagraph.text();
         let section;
-        for (const sectElement of sections) {
+        for (const sectElement of Object.entries(sections)) {
             const $element = $(sectElement);
             if ($element.hasClass('heading-large')) {
-                summary.pageTitle = $element.html();
+                summary.pageTitle = $element.text();
             }
-            if ($element.hasClass('heading-medium') || $element.hasClass('heading-small')) {
-                section = buildSection(section, $element, summary);
+            if ($element.hasClass('heading-medium')) {
+                section = buildSection(section, $element, summary, 'heading-medium');
+            }
+            if ($element.hasClass('heading-small')) {
+                section = buildSection(section, $element, summary, 'heading-small');
             }
             if ($element.hasClass('check-your-answers__row') && $element.children().length > 0) {
                 buildQuestionAndAnswers($element, section);
@@ -35,23 +36,23 @@ function buildQuestionAndAnswers($element, section) {
     const answer = $element.children('.check-your-answers__answer');
     const questionAndAnswer = {};
 
-    questionAndAnswer.question = question.html();
+    questionAndAnswer.question = question.text();
     questionAndAnswer.answers = [];
     const children = answer.children('.check-your-answers__row');
     if (children.length > 0) {
-        for (const answerChild of children) {
+        for (const answerChild of Object.entries(children)) {
             questionAndAnswer.answers.push(answerChild.textContent);
         }
     } else {
-        questionAndAnswer.answers.push(answer.html());
+        questionAndAnswer.answers.push(answer.text());
     }
     section.questionAndAnswers.push(questionAndAnswer);
 }
 
-function buildSection(section, $element, summary) {
+function buildSection(section, $element, summary, className) {
     section = {};
-    section.title = $element.html();
-    section.type = $element.attr('class');
+    section.title = $element.text();
+    section.type = className;
     section.questionAndAnswers = [];
     summary.sections.push(section);
     return section;
