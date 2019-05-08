0fd1e068b5b69c2ba940158303596c51be29282d
Wivenhoe-HMCTS <jason.smith@HMCTS.NET>
2019-05-08 11:49:49 +0100

    PRO-4869: change submit-data.js and reformating

--- app/components/submit-data.js
+++ app/components/submit-data.js
@@ -6,18 +6,15 @@ const submitData = (ctx, formdata) => {
     const body = {'type': 'Caveat'};
     body.applicationId = formdata.applicationId;
 
-    const applicant = {};
-    body.applicant = applicant;
-    applicant.firstName = formdata.applicant.firstName;
-    applicant.lastName = formdata.applicant.lastName;
-    applicant.email = formdata.applicant.email;
-
+    body.applicant = {};
+    body.applicant.firstName = formdata.applicant.firstName;
+    body.applicant.lastName = formdata.applicant.lastName;
+    body.applicant.email = formdata.applicant.email;
     body.applicant.address = formdata.applicant.address;
 
-    const deceased = {};
-    body.deceased = deceased;
-    deceased.firstName = formdata.deceased.firstName;
-    deceased.lastName = formdata.deceased.lastName;
+    body.deceased = {};
+    body.deceased.firstName = formdata.deceased.firstName;
+    body.deceased.lastName = formdata.deceased.lastName;
 
     body.deceased.address = formdata.deceased.address;
 
@@ -32,22 +29,19 @@ const submitData = (ctx, formdata) => {
     }
 
     if (formdata.ccdCase) {
-        const ccdCase = {};
-        body.ccdCase = formdata.ccdCase;
-        ccdCase.id = formdata.ccdCase.id;
-        ccdCase.state = formdata.ccdCase.state;
+        body.ccdCase = {};
+        body.ccdCase.id = formdata.ccdCase.id;
+        body.ccdCase.state = formdata.ccdCase.state;
     }
 
-    const registry = {};
     if (formdata.registry) {
-        body.registry = registry;
+        body.registry = {};
         body.registry.name = formdata.registry.name;
     }
 
     if (formdata.payment) {
-        const payments = [];
-        payments.push(formdata.payment);
-        body.payments = payments;
+        body.payments = [];
+        body.payments.push(formdata.payment);
     }
 
     return body;
