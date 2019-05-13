be212abad30cd78e29bea41e0a05dde04bf2df13
Wivenhoe-HMCTS <jason.smith@HMCTS.NET>
2019-05-13 13:07:58 +0100

    PRO-4869: changing function-tests.sh

--- bin/run-functional-tests.sh
+++ bin/run-functional-tests.sh
@@ -1,30 +1,16 @@
 #!/bin/bash
 set -ex
 
-ls -lrt
-pwd
-# cd probate-frontend
+export TEST_E2E_URL=$(echo ${TEST_URL})
 
-# X11 for Ubuntu is not configured! The following configurations are needed for XVFB.
-# Make a new display :21 with virtual screen 0 with resolution 1024x768 24dpi
-/bin/Xvfb :0 -screen 0 1024x768x24 -fbdir `pwd`
-#/bin/Xvfb :10 -screen 0 1920x1080x24 2>&1 >/dev/null &
-# Export the previously created display
-# export DISPLAY=:10.0
+if [ "$RUN_E2E_TEST" = true ] ;
+then
+    yarn test-e2e
+else
+    mkdir -p output
+    touch output/mochawesome.html
+fi
 
-# Right now this is not necessary, because of 'directConnect: true' in the 'e2e.conf.js'
-#echo "Starting webdriver"
-#node ./node_modules/protractor/bin/webdriver-manager start [OR webdriver-manager start] &
-#echo "Finished starting webdriver"
-printenv()
-# echo "Running functional tests"
-# The 'uluwatu-e2e-protractor' test project launch configuration file (e2e.conf.js) should be passed here.
-DISPLAY=:10 yarn run test-e2e 
-export RESULT=$?
-
-# echo "functional tests have done"
-# Close the XVFB display
-killall Xvfb
-
-exit $RESULT
-
+#below 2 lines for function test output in cnp pipeline
+mkdir -p functional-output
+cp -r output/mochawesome.* functional-output/. 2>/dev/null
