f33b4851505a0520b544a702dafa4d47c6ff6e68
Wivenhoe-HMCTS <jason.smith@HMCTS.NET>
2019-05-13 10:30:06 +0100

    PRO-4869 missing lable name and safe statement

--- test/config.js
+++ test/config.js
@@ -2,7 +2,7 @@ module.exports = {
 
     TestIdamBaseUrl: process.env.IDAM_API_URL || 'https://idam-api.aat.platform.hmcts.net',
     TestFrontendUrl: process.env.TEST_URL || 'http://localhost:3000',
-    TestE2EFrontendUrl: process.env.TEST_E2E_URL || 'https://probate.aat.platform.hmcts.net/caveats',
+    TestE2EFrontendUrl: process.env.TEST_E2E_URL || 'http://localhost:3000',
     TestUseSidam: process.env.USE_SIDAM || 'true',
     TestUseGovPay: process.env.USE_GOV_PAY || 'true',
     TestInviteIdListUrl: process.env.INVITE_ID_LIST_URL || '/inviteIdList',
