c522385b5fbc71268f2e3129dd7282fbfb12e1c6
Praveen Reddy <praveen.reddy@hmcts.net>
2019-05-14 11:41:44 +0100

    Testing e2e

--- bin/run-functional-tests.sh
+++ bin/run-functional-tests.sh
@@ -3,6 +3,11 @@ set -ex
 
 export TEST_E2E_URL=$(echo ${TEST_URL})
 
+if [[ "$TEST_URL" = "https://probate-caveats-fe-aat.service.core-compute-aat.internal" ]] ||  [[ "$TEST_URL" = "http://probate-caveats-fe-aat.service.core-compute-aat.internal" ]] ;
+then
+    export TEST_E2E_URL=$(echo "https://probate.aat.platform.hmcts.net")
+fi
+
 if [ "$RUN_E2E_TEST" = true ] ;
 then
     yarn test-e2e
