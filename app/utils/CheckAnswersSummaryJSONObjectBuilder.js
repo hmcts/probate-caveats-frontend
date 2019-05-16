b20013b8215a059efc1ffc034cac886f47305f0d
Wivenhoe-HMCTS <jason.smith@HMCTS.NET>
2019-05-16 13:23:47 +0100

    PRO-4869 replace .html() with .text()

--- app/utils/CheckAnswersSummaryJSONObjectBuilder.js
+++ app/utils/CheckAnswersSummaryJSONObjectBuilder.js
@@ -12,12 +12,12 @@ class CheckAnswersSummaryJSONObjectBuilder {
         summary.sections = [];
         const sections = $('.heading-large, .heading-medium, .heading-small, .check-your-answers__row');
         const mainParagraph = $('#main-heading-content');
-        summary.mainParagraph = mainParagraph.html();
+        summary.mainParagraph = mainParagraph.text();
         let section;
         for (const sectElement of sections) {
             const $element = $(sectElement);
             if ($element.hasClass('heading-large')) {
-                summary.pageTitle = $element.html();
+                summary.pageTitle = $element.text();
             }
             if ($element.hasClass('heading-medium') || $element.hasClass('heading-small')) {
                 section = buildSection(section, $element, summary);
@@ -35,7 +35,7 @@ function buildQuestionAndAnswers($element, section) {
     const answer = $element.children('.check-your-answers__answer');
     const questionAndAnswer = {};
 
-    questionAndAnswer.question = question.html();
+    questionAndAnswer.question = question.text();
     questionAndAnswer.answers = [];
     const children = answer.children('.check-your-answers__row');
     if (children.length > 0) {
@@ -43,14 +43,14 @@ function buildQuestionAndAnswers($element, section) {
             questionAndAnswer.answers.push(answerChild.textContent);
         }
     } else {
-        questionAndAnswer.answers.push(answer.html());
+        questionAndAnswer.answers.push(answer.text());
     }
     section.questionAndAnswers.push(questionAndAnswer);
 }
 
 function buildSection(section, $element, summary) {
     section = {};
-    section.title = $element.html();
+    section.title = $element.text();
     section.type = $element.attr('class');
     section.questionAndAnswers = [];
     summary.sections.push(section);
