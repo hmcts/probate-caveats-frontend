99b676787bddbbd7bdd368e09e3e03d069a81d14
Luca Del Buono <luca.delbuono@gmail.com>
2019-08-12 14:35:17 +0100

    DoB and DoD pages

--- test/unit/testDeceasedDob.js
+++ test/unit/testDeceasedDob.js
@@ -42,43 +42,43 @@ describe('DeceasedDob', () => {
         beforeEach(() => {
             session.form = {
                 deceased: {
-                    dod_day: '01',
-                    dod_month: '01',
-                    dod_year: '2000'
+                    'dod-day': '01',
+                    'dod-month': '01',
+                    'dod-year': '2000'
                 }
             };
         });
 
         it('should return the ctx with the deceased dob', (done) => {
             ctx = {
-                dob_day: '02',
-                dob_month: '03',
-                dob_year: '1952'
+                'dob-day': '02',
+                'dob-month': '03',
+                'dob-year': '1952'
             };
             errors = [];
             [ctx, errors] = DeceasedDob.handlePost(ctx, errors, formdata, session);
             expect(ctx).to.deep.equal({
-                dob_day: '02',
-                dob_month: '03',
-                dob_year: '1952'
+                'dob-day': '02',
+                'dob-month': '03',
+                'dob-year': '1952'
             });
             done();
         });
 
         it('should return the error for a date in the future', (done) => {
             ctx = {
-                dob_day: '02',
-                dob_month: '03',
-                dob_year: '3000'
+                'dob-day': '02',
+                'dob-month': '03',
+                'dob-year': '3000'
             };
             errors = [];
             [ctx, errors] = DeceasedDob.handlePost(ctx, errors, formdata, session);
             expect(errors).to.deep.equal([
                 {
-                    param: 'dob_date',
+                    param: 'dob-date',
                     msg: {
-                        summary: content.errors.dob_date.dateInFuture.summary,
-                        message: content.errors.dob_date.dateInFuture.message
+                        summary: content.errors['dob-date'].dateInFuture.summary,
+                        message: content.errors['dob-date'].dateInFuture.message
                     }
                 }
             ]);
@@ -87,18 +87,18 @@ describe('DeceasedDob', () => {
 
         it('should return the error for a date in the future', (done) => {
             ctx = {
-                dob_day: '02',
-                dob_month: '03',
-                dob_year: '3000'
+                'dob-day': '02',
+                'dob-month': '03',
+                'dob-year': '3000'
             };
             errors = [];
             [ctx, errors] = DeceasedDob.handlePost(ctx, errors, formdata, session);
             expect(errors).to.deep.equal([
                 {
-                    param: 'dob_date',
+                    param: 'dob-date',
                     msg: {
-                        summary: content.errors.dob_date.dateInFuture.summary,
-                        message: content.errors.dob_date.dateInFuture.message
+                        summary: content.errors['dob-date'].dateInFuture.summary,
+                        message: content.errors['dob-date'].dateInFuture.message
                     }
                 }
             ]);
@@ -107,18 +107,18 @@ describe('DeceasedDob', () => {
 
         it('should return the error for DoD before DoB', (done) => {
             ctx = {
-                dob_day: '02',
-                dob_month: '03',
-                dob_year: '2002'
+                'dob-day': '02',
+                'dob-month': '03',
+                'dob-year': '2002'
             };
             errors = [];
             [ctx, errors] = DeceasedDob.handlePost(ctx, errors, formdata, session);
             expect(errors).to.deep.equal([
                 {
-                    param: 'dob_date',
+                    param: 'dob-date',
                     msg: {
-                        summary: content.errors.dob_date.dodBeforeDob.summary,
-                        message: content.errors.dob_date.dodBeforeDob.message
+                        summary: content.errors['dob-date'].dodBeforeDob.summary,
+                        message: content.errors['dob-date'].dodBeforeDob.message
                     }
                 }
             ]);
@@ -132,19 +132,19 @@ describe('DeceasedDob', () => {
                 sessionID: 'dummy_sessionId',
                 session: {form: {}},
                 body: {
-                    dob_day: '02',
-                    dob_month: '03',
-                    dob_year: '1952'
+                    'dob-day': '02',
+                    'dob-month': '03',
+                    'dob-year': '1952'
                 }
             };
             const ctx = DeceasedDob.getContextData(req);
             expect(ctx).to.deep.equal({
-                dob_day: 2,
-                dob_month: 3,
-                dob_year: 1952,
-                dob_date: '1952-03-02T00:00:00.000Z',
-                dob_formattedDate: '2 March 1952',
-                sessionID: 'dummy_sessionId'
+                'dob-day': 2,
+                'dob-month': 3,
+                'dob-year': 1952,
+                'dob-date': '1952-03-02T00:00:00.000Z',
+                'dob-formattedDate': '2 March 1952',
+                'sessionID': 'dummy_sessionId'
             });
             done();
         });
--- test/unit/testDeceasedDod.js
+++ test/unit/testDeceasedDod.js
@@ -45,34 +45,34 @@ describe('DeceasedDod', () => {
 
         it('should return the ctx with the deceased dod', (done) => {
             ctx = {
-                dod_day: '02',
-                dod_month: '03',
-                dod_year: '1952'
+                'dod-day': '02',
+                'dod-month': '03',
+                'dod-year': '1952'
             };
             errors = [];
             [ctx, errors] = DeceasedDod.handlePost(ctx, errors, formdata, session);
             expect(ctx).to.deep.equal({
-                dod_day: '02',
-                dod_month: '03',
-                dod_year: '1952'
+                'dod-day': '02',
+                'dod-month': '03',
+                'dod-year': '1952'
             });
             done();
         });
 
         it('should return the error for a date in the future', (done) => {
             ctx = {
-                dod_day: '02',
-                dod_month: '03',
-                dod_year: '3000'
+                'dod-day': '02',
+                'dod-month': '03',
+                'dod-year': '3000'
             };
             errors = [];
             [ctx, errors] = DeceasedDod.handlePost(ctx, errors, formdata, session);
             expect(errors).to.deep.equal([
                 {
-                    param: 'dod_date',
+                    param: 'dod-date',
                     msg: {
-                        summary: content.errors.dod_date.dateInFuture.summary,
-                        message: content.errors.dod_date.dateInFuture.message
+                        summary: content.errors['dod-date'].dateInFuture.summary,
+                        message: content.errors['dod-date'].dateInFuture.message
                     }
                 }
             ]);
@@ -82,24 +82,24 @@ describe('DeceasedDod', () => {
         it('should return the error for DoD before DoB', (done) => {
             session.form = {
                 deceased: {
-                    dob_day: '02',
-                    dob_month: '03',
-                    dob_year: '2002'
+                    'dob-day': '02',
+                    'dob-month': '03',
+                    'dob-year': '2002'
                 }
             };
             ctx = {
-                dod_day: '01',
-                dod_month: '01',
-                dod_year: '2000'
+                'dod-day': '01',
+                'dod-month': '01',
+                'dod-year': '2000'
             };
             errors = [];
             [ctx, errors] = DeceasedDod.handlePost(ctx, errors, formdata, session);
             expect(errors).to.deep.equal([
                 {
-                    param: 'dod_date',
+                    param: 'dod-date',
                     msg: {
-                        summary: content.errors.dod_date.dodBeforeDob.summary,
-                        message: content.errors.dod_date.dodBeforeDob.message
+                        summary: content.errors['dod-date'].dodBeforeDob.summary,
+                        message: content.errors['dod-date'].dodBeforeDob.message
                     }
                 }
             ]);
