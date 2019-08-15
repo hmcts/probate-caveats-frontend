47a076e46bd4e3d7adc1574077e64ef386e20972
Luca Del Buono <luca.delbuono@gmail.com>
2019-08-15 09:51:12 +0100

    Caching options as a constant

--- app.js
+++ app.js
@@ -118,14 +118,16 @@ exports.init = function() {
 
     app.use(helmet.xssFilter({setOnOldIE: true}));
 
+    const caching = {cacheControl: true, setHeaders: (res) => res.setHeader('Cache-Control', 'max-age=604800')};
+
     // Middleware to serve static assets
-    app.use('/webchat', express.static(`${__dirname}/node_modules/@hmcts/ctsc-web-chat/assets`, {cacheControl: true, setHeaders: (res, path) => res.setHeader('Cache-Control', 'max-age=604800')}));
-    app.use('/public/stylesheets', express.static(`${__dirname}/public/stylesheets`, {cacheControl: true, setHeaders: (res, path) => res.setHeader('Cache-Control', 'max-age=604800')}));
-    app.use('/public/images', express.static(`${__dirname}/app/assets/images`, {cacheControl: true, setHeaders: (res, path) => res.setHeader('Cache-Control', 'max-age=604800')}));
-    app.use('/public/javascripts/govuk-frontend', express.static(`${__dirname}/node_modules/govuk-frontend`, {cacheControl: true, setHeaders: (res, path) => res.setHeader('Cache-Control', 'max-age=604800')}));
-    app.use('/public/javascripts', express.static(`${__dirname}/app/assets/javascripts`, {cacheControl: true, setHeaders: (res, path) => res.setHeader('Cache-Control', 'max-age=604800')}));
+    app.use('/webchat', express.static(`${__dirname}/node_modules/@hmcts/ctsc-web-chat/assets`, caching));
+    app.use('/public/stylesheets', express.static(`${__dirname}/public/stylesheets`, caching));
+    app.use('/public/images', express.static(`${__dirname}/app/assets/images`, caching));
+    app.use('/public/javascripts/govuk-frontend', express.static(`${__dirname}/node_modules/govuk-frontend`, caching));
+    app.use('/public/javascripts', express.static(`${__dirname}/app/assets/javascripts`, caching));
     app.use('/public/pdf', express.static(`${__dirname}/app/assets/pdf`));
-    app.use('/assets', express.static(`${__dirname}/node_modules/govuk-frontend/govuk/assets`, {cacheControl: true, setHeaders: (res, path) => res.setHeader('Cache-Control', 'max-age=604800')}));
+    app.use('/assets', express.static(`${__dirname}/node_modules/govuk-frontend/govuk/assets`, caching));
 
     // Elements refers to icon folder instead of images folder
     app.use(favicon(path.join(__dirname, 'node_modules', 'govuk-frontend', 'govuk', 'assets', 'images', 'favicon.ico')));
