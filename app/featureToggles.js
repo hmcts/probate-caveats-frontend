f3a22103a65f86f95d2580f6bae2b772c434b997
Luca Del Buono <luca.delbuono@gmail.com>
2019-09-02 10:37:05 +0100

    PRO-6048: Remove webchat feature toggle

--- app/featureToggles.js
+++ app/featureToggles.js
@@ -1,9 +1,5 @@
 'use strict';
 
 const router = require('express').Router();
-const FeatureToggle = require('app/utils/FeatureToggle');
-const featureToggle = new FeatureToggle();
-
-router.get('/*', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'webchat', featureToggle.toggleFeature));
 
 module.exports = router;
