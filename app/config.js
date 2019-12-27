6d959da99855f6c2b0d3a1200c755df18fb64952
Luca Del Buono <luca.delbuono@hmcts.net>
2019-12-27 15:26:36 +0000

    WLA-18: Make translations consistent between PRO/CAV

--- app/config.js
+++ app/config.js
@@ -112,7 +112,6 @@ module.exports = {
         whoInheritsLink: 'https://www.gov.uk/inherits-someone-dies-without-will',
         citizenAdvice: 'https://www.citizensadvice.org.uk/',
         stopGrantOfRepresentation: 'https://www.gov.uk/wills-probate-inheritance/stopping-a-grant-of-representation',
-        openGovernmentLicence: 'https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/',
         goodThingsFoundation: 'https://www.goodthingsfoundation.org',
         subjectAccessRequest: 'https://www.gov.uk/government/publications/request-your-personal-data-from-moj',
         complaintsProcedure: 'https://www.gov.uk/government/organisations/hm-courts-and-tribunals-service/about/complaints-procedure',
