dc40a5cd6236aa5bb4b84170153618cda0dc234f
Luca Del Buono <luca.delbuono@hmcts.net>
2020-01-28 12:34:49 +0000

    WLA-233: Upgrade Pa11y to latest version

--- test/util/a11y.js
+++ test/util/a11y.js
@@ -2,19 +2,16 @@
 
 const pa11y = require('pa11y');
 const console = require('console');
-const pa11yRun = pa11y({
-    hideElements: '.govuk-box-highlight, .govuk-header__logo, .govuk-footer, link[rel=mask-icon], .govuk-skip-link, .govuk-button--start, .govuk-visually-hidden, .govuk-warning-text__assistive',
-    log: {
-        error: console.error.bind(console)
-    }
-});
 
-module.exports = (testPage, title) => {
+module.exports = (testPage) => {
     return new Promise((resolve, reject) => {
-        pa11yRun.run(testPage, {
-            verifyPage: [
-                title
-            ],
+        pa11y(testPage, {
+            hideElements: '.govuk-box-highlight, .govuk-header__logo, .govuk-footer, link[rel=mask-icon], .govuk-skip-link, .govuk-button--start, .govuk-visually-hidden, .govuk-warning-text__assistive',
+            log: {
+                error: console.error.bind(console),
+                info: console.error.bind(console),
+                debug: console.error.bind(console)
+            }
         }, (err, results) => {
             if (err) {
                 reject(err);
