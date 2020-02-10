2e269aea59aa120f8e68acfa35f376d47841ec57
Luca Del Buono <luca.delbuono@hmcts.net>
2020-02-10 09:52:59 +0000

    WLA-18: Mark Language question as complete if FT off

--- app/steps/ui/language/index.js
+++ app/steps/ui/language/index.js
@@ -1,11 +1,16 @@
 'use strict';
 
 const ValidationStep = require('app/core/steps/ValidationStep');
+const featureToggle = require('app/utils/FeatureToggle');
 
 class BilingualGOP extends ValidationStep {
 
     static getUrl() {
         return '/bilingual-gop';
+    }
+
+    isComplete(ctx, formdata, featureToggles) {
+        return [ctx.bilingual || !featureToggle.isEnabled(featureToggles, 'welsh_ft'), 'inProgress'];
     }
 }
 
