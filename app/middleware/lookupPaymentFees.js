f54e2385064a6c88c68bee15b47389662b08ef6f
Luca Del Buono <luca.delbuono@hmcts.net>
2020-03-12 09:52:25 +0000

    Revert file

--- app/middleware/lookupPaymentFees.js
+++ app/middleware/lookupPaymentFees.js
@@ -1,48 +1,22 @@
 'use strict';
 
 const {set} = require('lodash');
+const formatUrl = require('app/utils/FormatUrl');
 const FeesLookup = require('app/utils/FeesLookup');
-const services = require('app/components/services');
-const security = require('app/components/security');
-const logger = require('app/components/logger');
-const logInfo = (message, applicationId = 'Unknown') => logger(applicationId).info(message);
 
 const lookupPaymentFees = (req, res, next) => {
     const session = req.session;
     const formdata = session.form;
     const applicationId = formdata.applicationId;
-    const feesLookup = new FeesLookup(applicationId);
+    const hostname = formatUrl.createHostname(req);
+    const feesLookup = new FeesLookup(applicationId, hostname);
 
-    services.authorise(req.session.form.applicationId)
-        .then(serviceAuthToken => {
-            if (serviceAuthToken.name === 'Error') {
-                logInfo('failed to obtain serviceAuthToken', req.session.form.applicationId);
-            } else {
-                req.serviceAuthToken = serviceAuthToken;
-
-                security.getUserToken(req.session.form.applicationId)
-                    .then(authToken => {
-                        if (authToken.name === 'Error') {
-                            logInfo('failed to obtain authToken', req.session.form.applicationId);
-                        } else {
-                            req.authToken = authToken;
-
-                            feesLookup.lookup(req.authToken)
-                                .then((res) => {
-                                    set(formdata, 'payment.total', res.total);
-                                    formdata.fees = res;
-                                    session.form = formdata;
-                                    next();
-                                });
-                        }
-                    })
-                    .catch(err => {
-                        logInfo('failed to obtain authToken', err);
-                    });
-            }
-        })
-        .catch(err => {
-            logInfo('failed to obtain serviceAuthToken', err);
+    feesLookup.lookup(req.authToken)
+        .then((res) => {
+            set(formdata, 'payment.total', res.total);
+            formdata.fees = res;
+            session.form = formdata;
+            next();
         });
 };
 
