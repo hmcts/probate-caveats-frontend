55ff4787645bd3ae91b1298bf5b4882259142945
Luca Del Buono <luca.delbuono@hmcts.net>
2020-03-12 10:08:39 +0000

    Remove unused hostname

--- app/middleware/lookupPaymentFees.js
+++ app/middleware/lookupPaymentFees.js
@@ -8,8 +8,7 @@ const lookupPaymentFees = (req, res, next) => {
     const session = req.session;
     const formdata = session.form;
     const applicationId = formdata.applicationId;
-    const hostname = formatUrl.createHostname(req);
-    const feesLookup = new FeesLookup(applicationId, hostname);
+    const feesLookup = new FeesLookup(applicationId);
 
     feesLookup.lookup(req.authToken)
         .then((res) => {
