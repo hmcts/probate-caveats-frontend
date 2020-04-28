571edeb6821b6260da2d99ca7f59459b6893624d
Luca Del Buono <luca.delbuono@hmcts.net>
2020-04-28 11:44:25 +0100

    PRO-7313: New Google Analytics tracking

--- app/setupSecrets.js
+++ app/setupSecrets.js
@@ -27,7 +27,6 @@ const setupSecrets = () => {
         setSecret('secrets.probate.probate-webchat-button-busy', 'webChat.buttonAgentsBusy');
         setSecret('secrets.probate.probate-webchat-button-service-closed', 'webChat.buttonServiceClosed');
         setSecret('secrets.probate.AppInsightsInstrumentationKey', 'appInsights.instrumentationKey');
-        setSecret('secrets.probate.google-analytics-tracking-id-caveats', 'gaTrackingId');
         setSecret('secrets.probate.launchdarkly-key', 'featureToggles.launchDarklyKey');
     }
 };
