a7dada7ff4fa7c2c1a98ca748844afae9356964c
Luca Del Buono <luca.delbuono@hmcts.net>
2020-04-29 13:18:32 +0100

    PRO-7210: Date error

--- app/core/steps/DateStep.js
+++ app/core/steps/DateStep.js
@@ -4,6 +4,7 @@ const ValidationStep = require('app/core/steps/ValidationStep');
 const moment = require('moment');
 const config = require('config');
 const utils = require('app/components/step-utils');
+const FieldError = require('app/components/error');
 
 class DateStep extends ValidationStep {
 
@@ -36,6 +37,19 @@ class DateStep extends ValidationStep {
         });
 
         return ctx;
+    }
+
+    validate(ctx, formdata, language) {
+        const dateName = this.dateName();
+        let [isValid, errors] = [true, []];
+
+        if (!ctx[`${dateName}-day`] && !ctx[`${dateName}-month`] && !ctx[`${dateName}-year`]) {
+            errors.push(FieldError(`${dateName}-date`, 'required', this.resourcePath, this.generateContent({}, {}, language), language));
+            isValid = false;
+        } else {
+            [isValid, errors] = super.validate(ctx, formdata, language);
+        }
+        return [isValid, errors];
     }
 }
 
