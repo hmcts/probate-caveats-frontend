961c37878a2dc15ae7307de9d08970a21693f66b
Luca Del Buono <luca.delbuono@hmcts.net>
2020-05-06 15:12:45 +0100

    PRO-7216: Opt out of LD diagnostic

--- app.js
+++ app.js
@@ -259,7 +259,7 @@ exports.init = function(isA11yTest = false, a11yTestSession = {}, ftValue) {
             };
         } else {
             res.locals.launchDarkly = {
-                client: LaunchDarkly.init(config.featureToggles.launchDarklyKey)
+                client: LaunchDarkly.init(config.featureToggles.launchDarklyKey, {diagnosticOptOut: true})
             };
         }
 
