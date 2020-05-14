9cc2e912f0ab7c43b259697c9ed8ba266e236f4a
Luca Del Buono <luca.delbuono@hmcts.net>
2020-05-14 16:07:02 +0100

    PRO-7216: Prepare for PCQ health check

--- app/middleware/completeEqualityTask.js
+++ app/middleware/completeEqualityTask.js
@@ -2,30 +2,36 @@
 
 const config = require('config');
 const get = require('lodash').get;
+const uuidv4 = require('uuid/v4');
 // const Healthcheck = require('app/utils/Healthcheck');
 // const logger = require('app/components/logger')('Init');
-const uuidv4 = require('uuid/v4');
 
 const completeEqualityTask = (params) => {
-    // const healthcheck = new Healthcheck();
-    // const services = [
-    //     {name: config.services.equalityAndDiversity.name, url: config.services.equalityAndDiversity.url}
-    // ];
+    // if (params.isEnabled && !get(params.req.session.form, 'equality.pcqId', false)) {
+    //     const healthcheck = new Healthcheck();
+    //     const services = [
+    //         {name: config.services.equalityAndDiversity.name, url: config.services.equalityAndDiversity.url}
+    //     ];
     //
-    // healthcheck.getDownstream(services, healthcheck.health, healthDownstream => {
-    //     params.req.session.equalityHealth = healthDownstream[0].status;
-    //     logger.info(config.services.equalityAndDiversity.name, 'is', params.req.session.equalityHealth);
+    //     healthcheck.getDownstream(services, healthcheck.health, healthDownstream => {
+    //         params.req.session.equalityHealth = healthDownstream[0].status;
+    //         logger.info(config.services.equalityAndDiversity.name, 'is', params.req.session.equalityHealth);
     //
-    //     if (params.req.session.equalityHealth === 'UP') {
-    //         params.req.session.form.equality = {
-    //             pcqId: uuidv4()
-    //         };
+    //         if (params.req.session.equalityHealth === 'UP') {
+    //             params.req.session.form.equality = {
+    //                 pcqId: uuidv4()
+    //             };
     //
-    //         params.next();
-    //     } else {
-    //         params.res.redirect(`${config.app.basePath}/summary`);
-    //     }
-    // });
+    //             params.next();
+    //         } else {
+    //             params.req.session.form.equality = {
+    //                 pcqId: 'Service down'
+    //             };
+    //
+    //             params.res.redirect(`${config.app.basePath}/summary`);
+    //         }
+    //     });
+    // }
 
     if (params.isEnabled && !get(params.req.session.form, 'equality.pcqId', false)) {
         params.req.session.form.equality = {
@@ -34,6 +40,10 @@ const completeEqualityTask = (params) => {
 
         params.next();
     } else {
+        params.req.session.form.equality = {
+            pcqId: 'Service down'
+        };
+
         params.res.redirect(`${config.app.basePath}/summary`);
     }
 };
