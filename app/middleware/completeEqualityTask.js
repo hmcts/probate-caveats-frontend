deff90b9845e0bebbe692e20564e2522f389fc28
Luca Del Buono <luca.delbuono@hmcts.net>
2020-05-18 15:03:55 +0100

    PRO-7216: Re-enable PCQ health check

--- app/middleware/completeEqualityTask.js
+++ app/middleware/completeEqualityTask.js
@@ -3,53 +3,53 @@
 const config = require('config');
 const get = require('lodash').get;
 const uuidv4 = require('uuid/v4');
-// const Healthcheck = require('app/utils/Healthcheck');
-// const logger = require('app/components/logger')('Init');
+const Healthcheck = require('app/utils/Healthcheck');
+const logger = require('app/components/logger')('Init');
 
 const completeEqualityTask = (params) => {
-    // if (params.isEnabled && !get(params.req.session.form, 'equality.pcqId', false)) {
-    //     const healthcheck = new Healthcheck();
-    //     const services = [
-    //         {name: config.services.equalityAndDiversity.name, url: config.services.equalityAndDiversity.url}
-    //     ];
-    //
-    //     healthcheck.getDownstream(services, healthcheck.health, healthDownstream => {
-    //         params.req.session.equalityHealth = healthDownstream[0].status;
-    //         logger.info(config.services.equalityAndDiversity.name, 'is', params.req.session.equalityHealth);
-    //
-    //         if (params.req.session.equalityHealth === 'UP') {
-    //             params.req.session.form.equality = {
-    //                 pcqId: uuidv4()
-    //             };
-    //
-    //             params.next();
-    //         } else {
-    //             params.req.session.form.equality = {
-    //                 pcqId: 'Service down'
-    //             };
-    //
-    //             params.res.redirect(`${config.app.basePath}/summary`);
-    //         }
-    //     });
-    // }
-
     if (params.isEnabled && !get(params.req.session.form, 'equality.pcqId', false)) {
-        params.req.session.form.equality = {
-            pcqId: uuidv4()
-        };
+        const healthcheck = new Healthcheck();
+        const services = [
+            {name: config.services.equalityAndDiversity.name, url: config.services.equalityAndDiversity.url}
+        ];
 
-        params.next();
-    } else {
-        if (!params.req.session.form) {
-            params.req.session.form = {};
-        }
+        healthcheck.getDownstream(services, healthcheck.health, healthDownstream => {
+            params.req.session.equalityHealth = healthDownstream[0].status;
+            logger.info(config.services.equalityAndDiversity.name, 'is', params.req.session.equalityHealth);
 
-        params.req.session.form.equality = {
-            pcqId: 'Service down'
-        };
+            if (params.req.session.equalityHealth === 'UP') {
+                params.req.session.form.equality = {
+                    pcqId: uuidv4()
+                };
 
-        params.res.redirect(`${config.app.basePath}/summary`);
+                params.next();
+            } else {
+                params.req.session.form.equality = {
+                    pcqId: 'Service down'
+                };
+
+                params.res.redirect(`${config.app.basePath}/summary`);
+            }
+        });
     }
+
+    // if (params.isEnabled && !get(params.req.session.form, 'equality.pcqId', false)) {
+    //     params.req.session.form.equality = {
+    //         pcqId: uuidv4()
+    //     };
+    //
+    //     params.next();
+    // } else {
+    //     if (!params.req.session.form) {
+    //         params.req.session.form = {};
+    //     }
+    //
+    //     params.req.session.form.equality = {
+    //         pcqId: 'Service down'
+    //     };
+    //
+    //     params.res.redirect(`${config.app.basePath}/summary`);
+    // }
 };
 
 module.exports = completeEqualityTask;
