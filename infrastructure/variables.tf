34cf8e7613e3a78bc597a81478b363a2a18e4361
Linus Norton <linusnorton@gmail.com>
2020-10-02 10:57:22 +0100

    cleanup

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -16,8 +16,7 @@ variable "subscription" {}
 
 // CNP settings
 variable "jenkins_AAD_objectId" {
-  type                        = "string"
-  description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
 variable "common_tags" {
