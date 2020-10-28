598a2972b471b6b3ade616db71d77fa75a3a3062
Elviani Huseyin <elviani.huseyin1@hmcts.net>
2020-10-28 14:55:43 +0000

    adding avaya webchat

--- app/featureToggles.js
+++ app/featureToggles.js
@@ -6,5 +6,6 @@ const featureToggle = new FeatureToggle();
 const completeEqualityTask = require('app/middleware/completeEqualityTask');
 
 router.get('/equality-and-diversity', (req, res, next) => featureToggle.callCheckToggle(req, res, next, res.locals.launchDarkly, 'ft_pcq', completeEqualityTask));
+router.get('/*', (req, res, next) => featureToggle.callCheckToggle(req, res, next, res.locals.launchDarkly, 'ft_avaya_webchat', featureToggle.toggleFeature));
 
 module.exports = router;
