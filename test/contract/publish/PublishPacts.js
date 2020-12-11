faaa38c4dbaebccb29bfd2df7a66d42a94037415
ruban72 <ruban72@hotmail.com>
2020-12-11 18:01:00 +0000

    TA-187: Initial setup of Pact payment tests

---
+++ test/contract/publish/PublishPacts.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/* eslint no-console: 0 */
+
+const path = require('path');
+const pact = require('@pact-foundation/pact-node');
+const config = require('config');
+const git = require('git-rev-sync');
+
+const opts = {
+    pactFilesOrDirs: [path.resolve(process.cwd(), config.services.pact.pactDirectory)],
+    pactBroker: config.services.pact.url,
+    consumerVersion: git.short(),
+    tags: config.services.pact.tag || 'Dev'
+};
+pact.publishPacts(opts)
+    .then(() => {
+        console.log('Pact contract publishing complete!');
+    })
+    .catch(e => {
+        console.log('Pact contract publishing failed: ', e);
+
+    });
