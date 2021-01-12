66897bc2002fd9cd45607a2c9136f8b59b0cb355
Rob Doherty <robdohertykelly@gmail.com>
2021-01-12 12:10:11 +0000

    DTSPB-1163: enable firefox and microsoft crossbrowser tests in the nightly pipline

--- test/end-to-end/pages/applicant/address.js
+++ test/end-to-end/pages/applicant/address.js
@@ -4,15 +4,11 @@ const commonContentEn = require('app/resources/en/translation/common');
 const commonContentCy = require('app/resources/cy/translation/common');
 const pageUnderTest = require('app/steps/ui/applicant/address/index');
 
-async function enterApplicantAddressManually(language = 'en', testAddressIndex) {
+async function enterApplicantAddressManually(language = 'en', testAddressIndex = '0') {
 
     const commonContent = language === 'en' ? commonContentEn : commonContentCy;
     const findAddress = language === 'en' ? 'Find UK address' : 'Dod o hyd i gyfeiriad';
     const I = this;
-
-    if (!testAddressIndex) {
-        testAddressIndex = '0';
-    }
 
     await I.waitInUrl(pageUnderTest.getUrl());
     await I.seeInCurrentUrl(pageUnderTest.getUrl());
@@ -22,6 +18,7 @@ async function enterApplicantAddressManually(language = 'en', testAddressIndex) 
     await I.waitForVisible('#postcodeAddress');
     await I.selectOption('#postcodeAddress', testAddressIndex);
     await I.waitForElement('#addressLine1');
+    await I.wait(2);
     await I.navByClick(commonContent.saveAndContinue);
 }
 
