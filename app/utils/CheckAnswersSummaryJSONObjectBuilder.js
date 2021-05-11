2ff78fa3ed070532b9153fea607cf82db7ec09ed
Elviani Huseyin <elviani.huseyin1@hmcts.net>
2021-05-11 12:04:05 +0100

    comment out some test that break the PR

--- app/utils/CheckAnswersSummaryJSONObjectBuilder.js
+++ app/utils/CheckAnswersSummaryJSONObjectBuilder.js
@@ -30,7 +30,7 @@ class CheckAnswersSummaryJSONObjectBuilder {
             }
             console.log('ELEMENT => ', $element);
             console.log('hasClass=> ', $element.hasClass('govuk-summary-list__row'));
-            console.log('children=> ', $element.children());
+            // console.log('children=> ', $element.children());
             if ($element && $element.hasClass('govuk-summary-list__row') && $element.children() && $element.children().length > 0) {
                 buildQuestionAndAnswers($element, section);
             }
