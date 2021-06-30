f12d629ae27e04ec0643095de31cef09eb77721b
Elviani Huseyin <elviani.huseyin1@hmcts.net>
2021-06-30 13:07:37 +0100

    block the locale url to go thrrough the router and app.js

--- app.js
+++ app.js
@@ -182,6 +182,10 @@ exports.init = function(isA11yTest = false, a11yTestSession = {}, ftValue) {
     // Send assetPath to all views
     app.use((req, res, next) => {
         res.locals.asset_path = `${globals.basePath}/public/`;
+        console.log('res => ', req.originalUrl);
+        if (!req.originalUrl.match(/locale/g)) {
+            next();
+        }
         next();
     });
 
