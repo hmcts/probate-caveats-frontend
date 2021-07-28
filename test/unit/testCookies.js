a017612ca53b1fc9317de958cc5c0bfb889057c7
Elviani Huseyin <elviani.huseyin1@hmcts.net>
2021-07-28 15:22:20 +0100

    DTSPB-1966: fixing the unit test error

--- test/unit/testCookies.js
+++ test/unit/testCookies.js
@@ -2,7 +2,7 @@
 
 const initSteps = require('app/core/initSteps');
 const expect = require('chai').expect;
-const steps = initSteps([`${__dirname}/../../../app/steps/action/`, `${__dirname}/../../../app/steps/ui`]);
+const steps = initSteps([`${__dirname}/../../app/steps/action/`, `${__dirname}/../../app/steps/ui`]);
 const Cookies = steps.Cookies;
 
 describe('Cookies', () => {
