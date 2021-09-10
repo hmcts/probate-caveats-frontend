8bee393e5c11974b92118ea6736cb88b4bb00599
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2021-09-10 15:04:11 +0100

    PCQ-1068 remove E2E tests

--- test/end-to-end/paths/caveat.e2e.js
+++ test/end-to-end/paths/caveat.e2e.js
@@ -1,4 +1,3 @@
-const testConfigurator = new (require('test/end-to-end/helpers/TestConfigurator'))();
 const languages = ['en', 'cy'];
 
 Feature('Standard Caveat E2E...').retry(2);
@@ -8,29 +7,29 @@ languages.forEach(language => {
     Scenario(`${language.toUpperCase()} - Caveat E2E`, async function ({I}) {
         await startApplicationToApplicantAddress(I, language);
         await I.enterDeceasedName(language, 'Deceased First Name', 'Deceased Last Name');
-        await I.enterDeceasedDateOfDeath(language, '01', '01', '2019');
-        await I.enterDeceasedDateOfBirthKnown(language);
-        await I.enterDeceasedDateOfBirth(language, '01', '01', '1977');
-        await I.enterDeceasedHasAlias(language);
-        await I.enterDeceasedOtherNames(language, 2);
-        await I.enterDeceasedAddressManually(language);
-
-        await I.selectBilingualGopNo(language);
-        if (testConfigurator.equalityAndDiversityEnabled) {
-            await I.completeEquality(language);
-        }
-
-        await I.seeSummaryPage(language);
-        await I.seePaymentBreakdownPage(language);
-        if (await testConfigurator.getUseGovPay() === 'true') {
-            await I.seeGovUkPaymentPage(language);
-            await I.seeGovUkConfirmPage();
-        }
-
-        const caseIDGenerated = await I.checkElementExist('//*[@id="main-content"]/div/div/div[1]/h1');
-        if (caseIDGenerated) {
-            await I.seeThankYouPage(language);
-        }
+        // await I.enterDeceasedDateOfDeath(language, '01', '01', '2019');
+        // await I.enterDeceasedDateOfBirthKnown(language);
+        // await I.enterDeceasedDateOfBirth(language, '01', '01', '1977');
+        // await I.enterDeceasedHasAlias(language);
+        // await I.enterDeceasedOtherNames(language, 2);
+        // await I.enterDeceasedAddressManually(language);
+        //
+        // await I.selectBilingualGopNo(language);
+        // if (testConfigurator.equalityAndDiversityEnabled) {
+        //     await I.completeEquality(language);
+        // }
+        //
+        // await I.seeSummaryPage(language);
+        // await I.seePaymentBreakdownPage(language);
+        // if (await testConfigurator.getUseGovPay() === 'true') {
+        //     await I.seeGovUkPaymentPage(language);
+        //     await I.seeGovUkConfirmPage();
+        // }
+        //
+        // const caseIDGenerated = await I.checkElementExist('//*[@id="main-content"]/div/div/div[1]/h1');
+        // if (caseIDGenerated) {
+        //     await I.seeThankYouPage(language);
+        // }
 
     }).tag('@e2e')
         .tag('@nightly')
