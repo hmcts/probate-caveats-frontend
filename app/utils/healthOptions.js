cae99ebc4ad7745be4f3d3ad062f0190e7313538
DelythJustice <delyth.jones11@justice.gov.uk>
2022-08-17 16:17:22 +0100

    DTSPB-2848 healthOptions was written badly needed fix to work and upped timeouts to 30seconds

--- app/utils/healthOptions.js
+++ app/utils/healthOptions.js
@@ -4,17 +4,15 @@ const healthcheck = require('@hmcts/nodejs-healthcheck');
 const config = require('config');
 const logger = require('app/components/logger')('Init');
 
-const healthOptions = function() {
-    return {
-        callback: (err, res) => {
-            if (err) {
-                logger.info('Health check failed!');
-            }
-            return res.body.status === 'good' ? healthcheck.up() : healthcheck.down();
-        },
-        timeout: config.health.timeout,
-        deadline: config.health.deadline
-    };
+const healthOptions = {
+    callback: (error, res) => { // eslint-disable-line id-blacklist
+        if (error) {
+            logger.info('Health check failed!');
+        }
+        return res.body.status === 'good' ? healthcheck.up() : healthcheck.down();
+    },
+    timeout: config.health.timeout,
+    deadline: config.health.deadline
 };
 
 module.exports = healthOptions;
