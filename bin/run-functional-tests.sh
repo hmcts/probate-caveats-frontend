332f2ed8fab1828044520c3c607dfa4badb2deb2
nilay <nilay.patel@hmcts.net>
2023-02-07 18:04:08 +0000

    Functional test fix for staging env

--- bin/run-functional-tests.sh
+++ bin/run-functional-tests.sh
@@ -7,7 +7,11 @@ if [[ "$TEST_URL" = "https://probate-caveats-fe-aat.service.core-compute-aat.int
         [[ "$TEST_URL" = "https://probate-caveats-fe-aat.service.core-compute-aat.internal/" ]] ||
         [[ "$TEST_URL" = "https://probate-caveats-fe-aat-staging.service.core-compute-aat.internal" ]] ||
         [[ "$TEST_URL" = "https://probate-caveats-fe-aat-staging.service.core-compute-aat.internal/" ]] ||
-        [[ "$TEST_URL" = "http://probate-caveats-fe-aat-staging.service.core-compute-aat.internal" ]] ;
+        [[ "$TEST_URL" = "http://probate-caveats-fe-aat-staging.service.core-compute-aat.internal" ]] ||
+        [[ "$TEST_URL" = "https://probate-caveats-fe-staging.service.core-compute-aat.internal" ]] ||
+        [[ "$TEST_URL" = "https://probate-caveats-fe-staging.service.core-compute-aat.internal/" ]] ||
+        [[ "$TEST_URL" = "http://probate-caveats-fe-staging.service.core-compute-aat.internal" ]] ||
+        [[ "$TEST_URL" = "http://probate-caveats-fe-staging.service.core-compute-aat.internal/" ]];
 then
     export TEST_E2E_URL=$(echo "https://probate.aat.platform.hmcts.net")
 fi
