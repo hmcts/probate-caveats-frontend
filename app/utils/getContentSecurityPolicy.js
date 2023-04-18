93b7c5d6783133ed2b0e0d32a3cd6ebb0e7b72bf
FeliTam <feli.tam@justice.gov.uk>
2023-04-18 16:28:49 +0100

    DTSPB-3513 update google analytics url

--- app/utils/getContentSecurityPolicy.js
+++ app/utils/getContentSecurityPolicy.js
@@ -11,8 +11,8 @@ const getContentSecurityPolicy = (nonce) => (
                 '\'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU=\'',
                 '\'sha256-AaA9Rn5LTFZ5vKyp3xOfFcP4YbyOjvWn2up8IKHVAKk=\'',
                 '\'sha256-G29/qSW/JHHANtFhlrZVDZW1HOkCDRc78ggbqwwIJ2g=\'',
-                'www.google-analytics.com',
-                'www.googletagmanager.com',
+                '*.google-analytics.com',
+                '*.googletagmanager.com',
                 `'nonce-${nonce}'`,
                 'webchat.ctsc.hmcts.net',
                 'webchat.pp.ctsc.hmcts.net',
@@ -21,8 +21,8 @@ const getContentSecurityPolicy = (nonce) => (
             ],
             connectSrc: [
                 '\'self\'',
-                'www.google-analytics.com',
-                'stats.g.doubleclick.net',
+                '*.google-analytics.com',
+                '*.g.doubleclick.net',
                 'tagmanager.google.com',
                 'https://webchat.ctsc.hmcts.net',
                 'https://webchat-client.ctsc.hmcts.net',
@@ -37,8 +37,8 @@ const getContentSecurityPolicy = (nonce) => (
             imgSrc: [
                 '\'self\'',
                 '\'self\' data:',
-                'www.google-analytics.com',
-                'stats.g.doubleclick.net',
+                '*.google-analytics.com',
+                '*.g.doubleclick.net',
                 'ssl.gstatic.com',
                 'www.gstatic.com',
                 'lh3.googleusercontent.com'
