7091bc07661b92870fd9f8dca7e16d8fd5357877
marian.irudayanathan <marian.irudayanathan@justice.gov.uk>
2026-02-02 12:06:39 +0000

    Update BROWSER_GROUP from 'safari' to 'webkit'

--- test/end-to-end/codecept.conf.js
+++ test/end-to-end/codecept.conf.js
@@ -61,7 +61,7 @@ exports.config = {
     multiple: {
         parallel: {
             chunks: 2,
-            browsers: ['chrome']
+            browsers: ['chrome', 'firefox', 'edge_win_latest', 'webkit']
         }
     },
     'name': 'Caveat E2E Tests'
