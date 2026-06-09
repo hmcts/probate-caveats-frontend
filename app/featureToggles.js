32799328924bfaae6241d4a4b469826550fdc62c
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2026-06-09 15:18:54 +0100

    DTSPB-5663 Probate Fee Increase 2026

--- app/featureToggles.js
+++ app/featureToggles.js
@@ -6,6 +6,7 @@ const featureToggle = new FeatureToggle();
 const completeEqualityTask = require('app/middleware/completeEqualityTask');
 
 router.get('/equality-and-diversity', (req, res, next) => featureToggle.callCheckToggle(req, res, next, res.locals.launchDarkly, 'ft_pcq', completeEqualityTask));
+router.get('/start-apply', (req, res, next) => featureToggle.callCheckToggle(req, res, next, res.locals.launchDarkly, 'ft_probate_fee_increase_2026', featureToggle.toggleFeature));
 router.get('/*', (req, res, next) => featureToggle.callCheckToggle(req, res, next, res.locals.launchDarkly, 'ft_enable_webchat', featureToggle.toggleFeature));
 
 module.exports = router;
